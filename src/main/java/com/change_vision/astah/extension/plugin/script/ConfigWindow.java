af52f5e696c75d52256a6adade7ccbca7ffd0b05
Takahiro Fujito <takahiro.fujito@change-vision.com>
2016-12-22 01:57:48 +0900

    #14 レイアウトの変更をし、表示するメッセージを追加した

--- src/main/java/com/change_vision/astah/extension/plugin/script/ConfigWindow.java
+++ src/main/java/com/change_vision/astah/extension/plugin/script/ConfigWindow.java
@@ -2,9 +2,11 @@ package com.change_vision.astah.extension.plugin.script;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JDialog;
@@ -16,15 +18,30 @@ public class ConfigWindow extends JDialog implements ActionListener {
     public ConfigWindow() {
         setLayout(new BorderLayout());
         
+        JPanel panel0 = new JPanel();
+        JPanel panel1 = new JPanel();
+        
+        
+        
+        panel0.setLayout(new GridBagLayout());
+        panel1.setLayout(new BoxLayout(panel1, BoxLayout.X_AXIS));
+        
+        add("South", panel0);
+        add("Center", panel1);
+        
+        
         JCheckBox ckbox = new JCheckBox();
-        ckbox = new JCheckBox("Clear");
-        add("North", ckbox);
+        ckbox = new JCheckBox(Messages.getMessage("config.clear_console"));
+           
+        JButton btn1 = new JButton(Messages.getMessage("config.ok.button"));
+        btn1.addActionListener(this);
         
-        JButton btn = new JButton("ok");
-        btn.addActionListener(this);
-        add("South", btn);
+        JButton btn2 = new JButton(Messages.getMessage("config.cancel.button"));
+        btn1.addActionListener(this);
         
-        
+        panel1.add(ckbox);
+        panel0.add(btn1);
+        panel0.add(btn2);
         
         setTitle(Messages.getMessage("action.config.label"));
         setSize(400, 300);
